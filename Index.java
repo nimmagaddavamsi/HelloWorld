4734f57e6bf46fc189df2efa33b29cf0e17b80b3
nimmagaddavamsi <96088807+nimmagaddavamsi@users.noreply.github.com>
2023-08-10 16:12:07 -0400

    Update Index.java

--- Index.java
+++ Index.java
@@ -1,55 +1,62 @@
-
-  To create a customized strategy for generating primary keys in Hibernate, you need to implement the `org.hibernate.id.IdentifierGenerator` interface. This interface has one method called `generate`, which you should override to provide your custom logic for generating primary key values.
-
-Here's a step-by-step guide on how to create a custom identifier generator in Hibernate:
-
-Step 1: Implement the IdentifierGenerator interface
-Create a class that implements the `IdentifierGenerator` interface. This class will define your custom logic to generate primary key values. The `generate` method should return a unique Serializable value that will be used as the primary key.
-
-```java
-import java.io.Serializable;
-import java.util.Random;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.hibernate.id.IdentifierGenerator;
+import org.hibernate.usertype.UserType;
 
-public class CustomPrimaryKeyGenerator implements IdentifierGenerator {
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class JsonbUserType implements UserType {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public Serializable generate(SharedSessionContractImplementor session, Object object) {
-        // Your custom logic to generate the primary key value
-        // For example, you can generate a random integer as the primary key
-        int primaryKeyValue = generateRandomInt();
-        return primaryKeyValue;
+    public int[] sqlTypes() {
+        return new int[]{Types.JAVA_OBJECT};
     }
 
-    private int generateRandomInt() {
-        Random random = new Random();
-        return random.nextInt(10000); // Change this as per your requirement
+    @Override
+    public Class returnedClass() {
+        return JsonNode.class;
     }
+
+    @Override
+    public Object nullSafeGet(
+            ResultSet resultSet,
+            String[] names,
+            SharedSessionContractImplementor session,
+            Object owner) throws HibernateException, SQLException {
+        String json = resultSet.getString(names[0]);
+        if (json == null) {
+            return null;
+        }
+        try {
+            return objectMapper.readTree(json);
+        } catch (Exception e) {
+            throw new HibernateException("Error converting JSONB column to JsonNode", e);
+        }
+    }
+
+    @Override
+    public void nullSafeSet(
+            PreparedStatement preparedStatement,
+            Object value,
+            int index,
+            SharedSessionContractImplementor session) throws HibernateException, SQLException {
+        if (value == null) {
+            preparedStatement.setNull(index, Types.OTHER);
+            return;
+        }
+        try {
+            preparedStatement.setObject(index, objectMapper.writeValueAsString(value), Types.OTHER);
+        } catch (Exception e) {
+            throw new HibernateException("Error converting JsonNode to JSONB column", e);
+        }
+    }
+
+    // Other methods (equals, hashCode, deepCopy, etc.) can be left as default or implemented as needed.
 }
-```
-
-Step 2: Annotate the entity class with your custom generator
-In your entity class, annotate the primary key field with the `@GeneratedValue` annotation, specifying your custom generator class in the `generator` attribute.
-
-```java
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class YourEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "custom-generator")
-    @org.hibernate.annotations.GenericGenerator(name = "custom-generator", strategy = "your.package.CustomPrimaryKeyGenerator")
-    private Long id;
-
-    // Other fields, constructors, getters, setters, etc.
-}
-```
-
-Ensure to replace `your.package` with the actual package name where your `CustomPrimaryKeyGenerator` class is located.
-
-That's it! Now, when you save a new instance of `YourEntity`, Hibernate will invoke your custom identifier generator to generate a primary key value for the `id` field. In this example, we generated a random integer, but you can implement any custom logic based on your requirements, such as using a combination of the current timestamp, a sequence, or any other unique identifier generator logic.
