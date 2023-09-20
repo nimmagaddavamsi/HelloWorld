fb65108b479f5e684084e05c02f13917459b5044
nimmagaddavamsi <96088807+nimmagaddavamsi@users.noreply.github.com>
2023-09-20 11:22:14 -0500

    Update Index.java

--- Index.java
+++ Index.java
@@ -1,19 +1,21 @@
 import java.io.*;
-import java.net.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
 
-public class SharePointUploader {
+public class OneDriveUploader {
 
-    public static void uploadToSharePoint(String url, String username, String password, String fileName, byte[] data) {
+    public static void uploadToOneDrive(String accessToken, String driveId, String folderId, String fileName, byte[] data) {
         try {
-            String endpoint = url + "/_api/web/getfolderbyserverrelativeurl('/Shared Documents')/files/add(url='" + fileName + "',overwrite=true)";
+            String endpoint = "https://graph.microsoft.com/v1.0/me/drives/" + driveId + "/items/" + folderId + ":/" + fileName + ":/content";
 
             URL apiUrl = new URL(endpoint);
             HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
 
             // Set the necessary headers for authentication and content type
-            connection.setRequestProperty("Authorization", "Basic " + encodeBase64(username + ":" + password));
-            connection.setRequestProperty("Content-Type", "application/json;odata=verbose");
-            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Bearer " + accessToken);
+            connection.setRequestProperty("Content-Type", "application/octet-stream");
+            connection.setRequestMethod("PUT");
             connection.setDoOutput(true);
 
             OutputStream outputStream = connection.getOutputStream();
@@ -32,17 +34,17 @@ public class SharePointUploader {
         }
     }
 
-    private static String encodeBase64(String s) {
-        return java.util.Base64.getEncoder().encodeToString(s.getBytes());
-    }
-
     public static void main(String[] args) {
-        String url = "https://your-sharepoint-site.com";
-        String username = "your-username";
-        String password = "your-password";
+        String username = "YOUR_USERNAME";  // OneDrive username
+        String password = "YOUR_PASSWORD";  // OneDrive password
+        String driveId = "YOUR_DRIVE_ID";  // The ID of the OneDrive drive
+        String folderId = "YOUR_FOLDER_ID";  // The ID of the target folder
         String fileName = "sample.txt";
-        byte[] data = "Hello, SharePoint!".getBytes();
+        byte[] data = "Hello, OneDrive!".getBytes();
 
-        uploadToSharePoint(url, username, password, fileName, data);
+        String basicAuth = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
+        String accessToken = "Bearer " + basicAuth;
+
+        uploadToOneDrive(accessToken, driveId, folderId, fileName, data);
     }
 }
