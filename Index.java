0608322235d17f7fcc533be2939c82c7ea659292
nimmagaddavamsi <96088807+nimmagaddavamsi@users.noreply.github.com>
2023-10-04 22:08:06 -0500

    Update Index.java

--- Index.java
+++ Index.java
@@ -1,97 +1,68 @@
-<dependencies>
-    <dependency>
-        <groupId>org.springframework.boot</groupId>
-        <artifactId>spring-boot-starter-mail</artifactId>
-    </dependency>
-    <dependency>
-        <groupId>org.apache.pdfbox</groupId>
-        <artifactId>pdfbox</artifactId>
-        <version>2.0.30</version> <!-- Latest version as of my knowledge cutoff -->
-    </dependency>
-</dependencies>
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceBusMessageSender {
+
+    private final ServiceBusSenderClient senderClient;
+
+    public ServiceBusMessageSender(@Value("${azure.servicebus.connection-string}") String connectionString,
+                                   @Value("${azure.servicebus.queue-name}") String queueName) {
+        this.senderClient = new ServiceBusClientBuilder()
+                .connectionString(connectionString)
+                .sender()
+                .queueName(queueName)
+                .buildSenderClient();
+    }
+
+    public void sendMessage(String message) {
+        ServiceBusMessage serviceBusMessage = new ServiceBusMessage(message);
+        senderClient.sendMessage(serviceBusMessage);
+    }
+}
+
+---------------
 
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 @Service
-public class EmailService {
+public class YourService {
 
-    @Autowired
-    private JavaMailSender javaMailSender;
+    private final ServiceBusMessageSender messageSender;
 
-    public void sendEmailWithAttachment(String to, String subject, String body, byte[] pdfBytes) throws MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(body);
-
-        // Attach the PDF file
-        helper.addAttachment("attachment.pdf", new ByteArrayResource(pdfBytes));
-
-        javaMailSender.send(message);
+    public YourService(ServiceBusMessageSender messageSender) {
+        this.messageSender = messageSender;
     }
 
-    public byte[] generatePdf() throws IOException {
-        PDDocument document = new PDDocument();
-        PDPage page = new PDPage();
-        document.addPage(page);
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        document.save(byteArrayOutputStream);
-        document.close();
-
-        return byteArrayOutputStream.toByteArray();
+    public void sendToServiceBus(String message) {
+        messageSender.sendMessage(message);
     }
 }
 
+----------------
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import javax.mail.MessagingException;
-import java.io.IOException;
 
 @RestController
-public class EmailController {
+public class YourController {
 
-    @Autowired
-    private EmailService emailService;
+    private final YourService yourService;
 
-    @GetMapping("/sendEmail")
-    public String sendEmail() {
-        try {
-            String to = "recipient@example.com";
-            String subject = "Subject of the email";
-            String body = "Body of the email";
+    public YourController(YourService yourService) {
+        this.yourService = yourService;
+    }
 
-            byte[] pdfBytes = emailService.generatePdf();
-
-            emailService.sendEmailWithAttachment(to, subject, body, pdfBytes);
-
-            return "Email sent successfully.";
-        } catch (MessagingException | IOException e) {
-            e.printStackTrace();
-            return "Error sending email.";
-        }
+    @PostMapping("/send-message")
+    public void sendMessage(@RequestBody String message) {
+        yourService.sendToServiceBus(message);
     }
 }
 
+-----------
 
-spring.mail.host=your_smtp_host
-spring.mail.port=your_smtp_port
-spring.mail.username=your_email@example.com
-spring.mail.password=your_email_password
-spring.mail.properties.mail.smtp.auth=true
-spring.mail.properties.mail.smtp.starttls.enable=true
